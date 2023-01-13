077f302c032d144fbeb4260aa5561af8ea6278e8
GaelCruz <gaelecruz18@gmail.com>
2023-01-13 16:35:45 -0600

    created env file for github usernames API. Displays github usernames. vanilla

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import UserResults from "../components/users/UserResults"
 
 function Home() {
   return (
-    <div>
-      
-    </div>
+    <>
+      {/*seachr component*/}
+      <UserResults />
+    </>
   )
 }
 
